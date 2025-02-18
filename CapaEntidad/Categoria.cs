a362263ae6655400f896f4d7bcaf520eaf88dd45
Samary Cardozo <samarycardozo@gmail.com>
2025-02-18 11:04:22 -0500

    crear clase en entidad

---
+++ CapaEntidad/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+
+//    CREATE TABLE CATEGORIA(
+//    IdCategoria INT PRIMARY KEY IDENTITY,
+//    Descripcion VARCHAR(100),
+//    Activo BIT DEFAULT 1,
+//    FechaRegistro DATETIME DEFAULT GETDATE()
+//);
+//GO
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    public class Categoria
+    {
+
+
+    }
+}
